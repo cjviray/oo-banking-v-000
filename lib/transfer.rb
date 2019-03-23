24422516549048f412c55189164e01ae294d01a1
Christian V <viray.christianj@gmail.com>
2019-03-23 04:32:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
   
   
   def reverse_transfer
-    if @status = "complete" 
+    if @status == "complete" 
       sender.balance += amount
       receiver.balance -= amount
       @status = "reversed"
